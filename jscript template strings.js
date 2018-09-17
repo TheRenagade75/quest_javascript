66bf8f34bffb312290f1aff38bca43cef682f36d
Killian Rhelimi <41280091+TheRenagade75@users.noreply.github.com>
2018-09-17 14:45:23 +0200

    Add files via upload

---
+++ jscript template strings.js
@@ -0,0 +1,6 @@
+let city = "Tours";
+let age = "20";
+const dateOfBirth = "05/03/1998";
+
+let sentence = "J'habite à "+city+ " j'ai "+age+ " ans et je suis né(e) le "+dateOfBirth;
+console.log(sentence);
